97075601b11b390321e5d239c8a858db2804efba
Phil <57153492+PhilippFors@users.noreply.github.com>
2024-09-09 03:54:18 +0200

    Set godot sdk to 4.3.0

--- Node1.cs
+++ Node1.cs
@@ -4,11 +4,9 @@ namespace GDBridge_TEST;
 
 public partial class Node1 : Node
 {
-	// public NodeHelperBridge godotObject;
+
 	public override void _Ready()
 	{
-		
-		
-		// godotObject = new GdScriptBridgeFactory(this).ResolveBridge<NodeHelperBridge>("res://NodeHelper.gd");
+
 	}
 }
